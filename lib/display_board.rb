18b539853d59c28d1517ab4042c0c0906c14a34b
chic-page-6502 <zacharytq@gmail.com>
2019-07-09 23:22:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(state)
+  line_1 = " #{state[0]} | #{state[1]} | #{state[2]} "
+  line_2 = " #{state[3]} | #{state[4]} | #{state[5]} "
+  line_3 = " #{state[6]} | #{state[7]} | #{state[8]} "
+  seperators = "-----------"
+  puts line_1
+  puts seperators
+  puts line_2
+  puts seperators
+  puts line_3
+end
